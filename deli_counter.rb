0833771d61c3eecf06f05b350eebea945150fe28
roy renno <royrenno@yahoo.com>
2019-01-31 03:45:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,29 +1,29 @@
 # Write your code here.
-def line(linearr)
+def line(peps)
 	# Prints out the entire line with position and name
 	# if line empty, says so
-	if linearr.length == 0
+	if peps.length == 0
 	puts "The line is currently empty."
 	else
 	string = "The line is currently:"
-	(1..(linearr.length)).each do |num|
-	string = string + " #{num}. #{linearr[num - 1]}"
+	(1..(peps.length)).each do |num|
+	string = string + " #{num}. #{peps[num - 1]}"
 	end
 	puts string
 	end
 	end
-	def take_a_number(linearr, name)
+	def take_a_number(peps, name)
 	# adds person with name to back of linearr
 	# tells person their position in line
 	linearr << name
-	puts "Welcome, #{name}. You are number #{linearr.length} in line."
+	puts "Welcome, #{name}. You are number #{peps.length} in line."
 	end
-	def now_serving(linearr)
+	def now_serving(peps)
 	# calls next person in line and removes them from linearr
 	# if line empty, prints that nobody is waiting
-	if linearr.length == 0
+	if peps.length == 0
 	puts "There is nobody waiting to be served!"
 	else
-	puts "Currently serving #{linearr.shift}."
+	puts "Currently serving #{peps.shift}."
 	end
 	end
